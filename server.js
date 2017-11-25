44834a73ba8760e4763bbe465a5763a77b326757
Erik <ecc100@outlook.com>
2017-11-25 16:20:37 +0100

    Heroku config edded.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -34,9 +35,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs', appData);
-});
+//app.use((req, res, next) => {
+//  res.render('maintenance.hbs', appData);
+//});
 
 app.get('/', (req, res) => {
   res.render('index.hbs', appData);
@@ -53,6 +54,6 @@ app.get('/about', (req, res) => {
   res.render('about.hbs', appData);
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
