1ada3c53be235bc9f42200143e1769b4ab06d616
Erik <ecc100@outlook.com>
2017-11-25 15:44:32 +0100

    Commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+var appData = {
+  pageTitle: 'Lstn app',
+  appTitle: 'First Node lstn',
+  welcomeMessage: 'Welcome to the web!'
+};
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  //console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs', appData);
+});
+
+app.get('/', (req, res) => {
+  res.render('index.hbs', appData);
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 'fault bad',
+    code: 9292
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', appData);
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
